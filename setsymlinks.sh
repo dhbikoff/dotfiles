1b449382730b468276dda67f1fec5c0dedba4909
Derek Bikoff <dhbikoff@gmail.com>
2016-10-10 07:54:21 -0400

    Add script for symlinking config files

---
+++ setsymlinks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -s $(pwd)/.config/terminator/config $HOME/.config/terminator/config
+ln -s $(pwd)/.oh-my-zsh/custom/custrobbyrussell.zsh-theme $HOME/.oh-my-zsh/custom/custrobbyrussell.zsh-theme
+ln -s $(pwd)/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
+ln -s $(pwd)/.bashrc $HOME/.bashrc
+ln -s $(pwd)/.gitconfig $HOME/.gitconfig
+ln -s $(pwd)/.shared_config $HOME/.shared_config
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+ln -s $(pwd)/.zshrc $HOME/.zshrc
+
