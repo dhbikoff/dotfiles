848d00d817c0be329433e7404e1cae1f82194e23
Derek Bikoff <dhbikoff@gmail.com>
2016-10-22 08:07:27 -0400

    Force symlinks

--- setsymlinks.sh
+++ setsymlinks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-ln -s $(pwd)/.config/terminator/config $HOME/.config/terminator/config
-ln -s $(pwd)/.oh-my-zsh/custom/custrobbyrussell.zsh-theme $HOME/.oh-my-zsh/custom/custrobbyrussell.zsh-theme
-ln -s $(pwd)/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
-ln -s $(pwd)/.bashrc $HOME/.bashrc
-ln -s $(pwd)/.gitconfig $HOME/.gitconfig
-ln -s $(pwd)/.shared_config $HOME/.shared_config
-ln -s $(pwd)/.vimrc $HOME/.vimrc
-ln -s $(pwd)/.zshrc $HOME/.zshrc
+ln -s -f $(pwd)/.config/terminator/config $HOME/.config/terminator/config
+ln -s -f $(pwd)/.oh-my-zsh/custom/custrobbyrussell.zsh-theme $HOME/.oh-my-zsh/custom/custrobbyrussell.zsh-theme
+ln -s -f $(pwd)/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
+ln -s -f $(pwd)/.bashrc $HOME/.bashrc
+ln -s -f $(pwd)/.gitconfig $HOME/.gitconfig
+ln -s -f $(pwd)/.shared_config $HOME/.shared_config
+ln -s -f $(pwd)/.vimrc $HOME/.vimrc
+ln -s -f $(pwd)/.zshrc $HOME/.zshrc
 
