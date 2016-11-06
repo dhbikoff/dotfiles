326d9c9e77ef2f406cdd76b23ab1a0e99be5d8f9
Derek Bikoff <dhbikoff@gmail.com>
2016-11-06 12:50:07 -0500

    Remove symlinks to zsh config files

--- setsymlinks.sh
+++ setsymlinks.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 ln -s -f $(pwd)/.config/terminator/config $HOME/.config/terminator/config
-ln -s -f $(pwd)/.oh-my-zsh/custom/custrobbyrussell.zsh-theme $HOME/.oh-my-zsh/custom/custrobbyrussell.zsh-theme
 ln -s -f $(pwd)/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
 ln -s -f $(pwd)/.bashrc $HOME/.bashrc
 ln -s -f $(pwd)/.bash_profile $HOME/.bash_profile
 ln -s -f $(pwd)/.gitconfig $HOME/.gitconfig
 ln -s -f $(pwd)/.shared_config $HOME/.shared_config
 ln -s -f $(pwd)/.vimrc $HOME/.vimrc
-ln -s -f $(pwd)/.zshrc $HOME/.zshrc
 ln -s -f $(pwd)/.git_prompt.sh $HOME/.git_prompt.sh
 
