24e16389843f3721c4d8eed2e5878c576eeb460e
Derek Bikoff <dhbikoff@gmail.com>
2016-11-08 09:37:29 -0500

    Limit symlinks to OS type

--- setsymlinks.sh
+++ setsymlinks.sh
@@ -4,13 +4,15 @@ if [ "$(uname)" = "Darwin" ]; then
   ln -s -f $(pwd)/.bash_profile $HOME/.bash_profile
 fi
 
+if [ "$(uname)" = "Linux" ]; then
+  mkdir -p $HOME/.config/terminator
+  ln -s -f $(pwd)/.config/terminator/config $HOME/.config/terminator/config
+  ln -s -f $(pwd)/.bashrc $HOME/.bashrc
+fi
+
 mkdir -p $HOME/.vim/colors
 ln -s -f $(pwd)/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
 
-mkdir -p $HOME/.config/terminator
-ln -s -f $(pwd)/.config/terminator/config $HOME/.config/terminator/config
-
-ln -s -f $(pwd)/.bashrc $HOME/.bashrc
 ln -s -f $(pwd)/.gitconfig $HOME/.gitconfig
 ln -s -f $(pwd)/.shared_config $HOME/.shared_config
 ln -s -f $(pwd)/.vimrc $HOME/.vimrc
