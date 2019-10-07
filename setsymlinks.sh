6b0963bee921afd1c44ac44993dad70ac29a3747
Derek Bikoff <dbikoff@schoology.com>
2019-10-06 21:35:06 -0400

    unhide all files

--- setsymlinks.sh
+++ setsymlinks.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" = "Darwin" ]; then
-  ln -s -f $(pwd)/.bash_profile $HOME/.bash_profile
+  ln -s -f $(pwd)/bash_profile $HOME/.bash_profile
 fi
 
 if [ "$(uname)" = "Linux" ]; then
-  mkdir -p $HOME/.config/terminator
+  mkdir -p $HOME/config/terminator
   ln -s -f $(pwd)/terminator/config $HOME/.config/terminator/config
-  ln -s -f $(pwd)/.bashrc $HOME/.bashrc
+  ln -s -f $(pwd)/bashrc $HOME/.bashrc
 fi
 
-ln -s -f $(pwd)/.gitconfig $HOME/.gitconfig
-ln -s -f $(pwd)/.shared_config $HOME/.shared_config
-ln -s -f $(pwd)/.vimrc $HOME/.vimrc
-ln -s -f $(pwd)/.git_prompt.sh $HOME/.git_prompt.sh
+ln -s -f $(pwd)/gitconfig $HOME/.gitconfig
+ln -s -f $(pwd)/shared_config $HOME/.shared_config
+ln -s -f $(pwd)/vimrc $HOME/.vimrc
+ln -s -f $(pwd)/git_prompt.sh $HOME/.git_prompt.sh
 
